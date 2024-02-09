417222481d36bec4e57ddce119577e7c5be923e1
Rakshith Bellare <rb@nuiteq.com>
2024-02-09 21:46:49 +0530

    oushadhavana-base Using vue js for easy templating

--- oushadhavana/data.js
+++ oushadhavana/data.js
@@ -25,3 +25,76 @@ const data = [
   }
 ];
 
+const plants = [
+  {
+    "Number": 1,
+    "sanskrit_name": "Apamarga",
+    "Botanical name": "Achyranthes aspera L.",
+    "English name": "Prickly chaff flower",
+    "Malayalam Name": "Katalati കടലാടി",
+    "Kannada Name": "Utrani ಉತ್ತರಾಣಿ",
+    "Description": "Achyranthes aspera is an annual herb that can grow up to 1-2 meters in height. It has an erect, stout stem; simple, opposite, and oblong leaves; and small greenish flowers that form in axillary or terminal spikes. The seeds are encapsulated in a utricle, and the plant is distinguished by its rough texture.",
+    "Geographical distribution": "It is distributed throughout the tropical world. It can be found in many places growing as an introduced species and a common weed. It is an invasive species in some areas, including many Pacific Islands environments.",
+    "Chemical constituents": "Betaine, Achyranthine, Hentriacontane, Ecdysterone; achyranthes saponins A,B,C,D.",
+    "Important Therapeutic use": "Apamarga is an excellent diuretic. Hence useful in dysuria, urinary retention, water retention and urinary stones; It helps to break down the kidney and bladder stones. This action is not only due to its diuretic action.",
+    "Other medical uses": "Antihypertensive - to treat blood pressure\nAnti diabetic - to treat blood sugar\nAbortifacient - causing abortion\nDiuretic - to increase urine production\nPurgative - to loosen stool\nSpasmolytic - to relieve muscle stiffness Hypoglycemic - to decrease blood sugar\nAnti fungal - to treat fungal infection",
+    "Ayurvedic uses ": "Breaks down and expels sputum collection. Useful in piles, hemorrhoid's. Useful in relieving pruritus (excessive itching). Useful in ascites. Relieves ama - a product of indigestion and altered metabolism.\nImproves blood. An excess dose may cause nausea and vomiting. Improves digestion strength. It helps to relieve worms and infections in head and neck region. Relieves bloating, balances Vata Dosha. Relieves abdominal colic pain. Relieves hiccups. Useful in piles, hemorrhoid's. Ueful in urticaria, itching, allergic skin rashes. Useful in fibroids, glandular growths. Improves taste, relieves anorexia. Useful in cardiac disorders with cholesterol deposition in blood vessels. Relieves bloating, gaseous distension of abdomen. Fruit is sweet in taste, hard to undergo digestion, useful in bleeding disorders.",
+    "Google drive Album": "",
+    "Reference/Futher Reading Link": "https://www.easyayurveda.com/2014/11/20/apamarga-achyranthes-aspera-uses-dose-side-effects/#varieties_distribution",
+    "Adopter Name": "",
+    "Adopter contact": ""
+  },
+  {
+    "Number": 2,
+    "sanskrit_name": "Cluster fig",
+    "Botanical name": "Ficus racemosa L",
+    "English name": "Sadāphala",
+    "Malayalam Name": "Atthi",
+    "Kannada Name": "Atthimara",
+    "Description": "Cluster fig trees can grow up to 25-30 meters in height. They have a wide canopy with large, spreading branches. The leaves are broad, ovate, and have a glossy appearance. The fruit is small, round, and turns from green to red or purple as it ripens.",
+    "Geographical distribution": "It is predominantly found in South and Southeast Asia, notably in India, Sri Lanka, Bangladesh, and Malaysia, and extends to northern and eastern Australia, as well as parts of eastern and southern Africa",
+    "Chemical constituents": "Leaves & Fruits- Gluacol\nFruits- B- Sitosterol, ceryl behenate, Lupeol, Friedelin",
+    "Important Therapeutic use": "Cluster fig trees, belong to the Moraceae family. They are known for their unique fruiting habit; the fruits grow in clusters directly from the main trunk and larger branches of the tree, hence the name Cluster fig.",
+    "Other medical uses": "For Using on the Skin:\n\nIt can help with pain, heal wounds, and reduce swelling.\nThe sap from the plant helps with swelling, wounds, and more.\nThe young leaves can be put on the skin to make it look better.\nBoiling the leaves in water makes a liquid that can clean wounds and help with mouth problems when used as a mouthwash.\nFor Taking by Mouth:\nDigestive System:\nIt can soak up substances in the stomach, calming an upset stomach.\nEating the ripe fruit might make worms grow in the intestines.\nBoiling the bark in water helps with bleeding from the rear end, trouble absorbing nutrients from food, and more. The sap is also good for young children to help prevent issues when they are getting new teeth.\nBlood System:\nThe bark and fruit help stop bleeding.\nReproductive System:\nA special treatment using the bark helps with excessive vaginal discharge, heavy menstrual bleeding, and bleeding at unexpected times. It's also good for keeping a pregnancy healthy and helping the baby grow. The sap helps prevent releasing too soon during sex.\nPeeing System:\nIt makes you pee less and is good for diabetes when using the bark or fruit.\nFor Heat-Related Problems:\nEating the ripe fruit is good for conditions that make you feel like you're burning up."
+  },
+  {
+    "Number": 3,
+    "sanskrit_name": "Flame of the Forest",
+    "Botanical name": "Butea monosperma\n(Lam.) Kuntze",
+    "English name": "Palasa",
+    "Malayalam Name": "Chamata",
+    "Kannada Name": "Muttagamara",
+    "Description": "Butea monosperma can grow up to 15 meters tall. It has a crooked trunk and spreading branches, forming a broad crown. The leaves are pinnate, with three leaflets, and are shed during the winter months, rendering the tree leafless when it blooms in spring.",
+    "Geographical distribution": "It is primarily found in the Indian Subcontinent, including India, Nepal, Sri Lanka, Bangladesh, and Pakistan. It also grows in Southeast Asia, covering areas in Myanmar, Thailand, Laos, Cambodia, and Vietnam. Its distribution reflects a preference for tropical and subtropical environments, thriving in a variety of habitats such as forests, grasslands, and alongside roads and riverbanks",
+    "Chemical constituents": "Leaves of butea are rich in glucoside, linoleic acid, oleic acid and lignoceric acid. Bark contains gallic acid, palasitrin, butrin, butolic acid, cyaniding, Lupenone and palasimide. Gum contains tannins, pyrocatechin and mucil isaginous materials. Flowers are rich in flavonoids, triterpene, butein, butin, isobutrin, coreopsin, isocoreopsin and sulphurein.",
+    "Important Therapeutic use": "Commonly known as Flame of the Forest, Palash, or Bastard Teak, is a species of tree native to tropical and subtropical parts of the Indian Subcontinent and Southeast Asia. It belongs to the family Fabaceae, the pea family.",
+    "Other medical uses": "Wound Healing: Applied externally to heal cuts and bruises.\nAnti-inflammatory: Used to reduce inflammation and pain, beneficial in conditions like arthritis.\nAntioxidant: The flowers offer antioxidant benefits, protecting against cellular damage.\nDiuretic: Flowers help in promoting urination, aiding in detoxification and urinary tract health.\nAnti-diabetic: Flowers are used to help manage blood sugar levels.\nAnthelmintic: Seeds are effective against intestinal worms.\nContraceptive: Seeds have been used traditionally for their contraceptive properties.\nAstringent: The bark is used to treat diarrhea and dysentery, helping to control loose stools.\nSkin Diseases: Used in treating skin conditions due to antimicrobial and anti-inflammatory properties.\nEye Health: Employed in traditional remedies for improving eye health and treating infections.\nFever Reduction: Has antipyretic properties to help lower fever.\nDetoxification: Used in traditional medicine for blood purification and toxin elimination."
+  },
+  {
+    "Number": 4,
+    "sanskrit_name": "Aswaddha",
+    "Botanical name": "Ficus religiosa L",
+    "English name": "Sacred fig",
+    "Malayalam Name": "Ashwatham",
+    "Kannada Name": "Ashwatha",
+    "Description": "Peepal is unrivalled for its antiquity and religious significance. No other tree is claimed to have such long life - one in Sri Lanka, said to have been planted in the year 288 B.C., still lives and flourishes. The Prince Siddhartha is known to have sat in meditation under a Bo tree and there found enlightenment from which time he became known as the Buddha. So, from then on the tree was sacred to Buddhists. Hindus associate the tree with the three gods Brahma, Vishnu and Shiva, Vishnu being reputed to have been born under a Peepul, which is therefore Vishnu himself in the form of a tree. A grand peepal tree is a perfect shade tree, and village meetings are often conducted under a peepal tree. It is a large deciduous tree with a pale stem often appearing fluted on account of the numerous roots which have fused with the stem. Leaves leathery 4-8 inches long by 3-5 inches wide, somewhat egg-shaped or rounded, tailed at the tip and heart-shaped at the base, or sometimes rounded. The young leaves are frequently pink, change to copper and finally to green. Flowers minute within the receptacle. Fruit is a fig.",
+    "Geographical distribution": " It is geographically distributed primarily in the Indian subcontinent, extending to parts of Southeast Asia. Its native range includes countries such as India, Nepal, Bangladesh, Pakistan, Sri Lanka, southwest China, and Indochina (including Thailand, Vietnam, and Cambodia). Due to its cultural significance and adaptability, it has also been introduced and naturalized in other tropical and subtropical regions around the world. The tree thrives in a variety of climatic conditions, from temperate to tropical environments, making it a common sight in both rural and urban landscapes within its native and introduced ranges.",
+    "Important Therapeutic use": "Ashwattha (Peepal tree) is a well known religious tree appreciated all over India. Ayurveda uses stem bark, fruits, root, leaf, milk, tender shoots, in various medicines and remedies.\nAshwattha tree is well known for its wound healing, absorbent, memory enhancing and complexion enhancing properties. As per astrology and Hindu philosophy it is useful in infertility cases as well.",
+    "Other medical uses": "The tree is useful in the treatment of chronic ulcers, stomatitis, menstrual irregularities, diarrhea, diabetes, epilepsy, and gastric disturbances, infertility and infectious disorders.\n• The milky latex obtained from the bark, root or fruit of the plant is given in a dose of 5-6 drops with honey and ghee to treat impotence and help in conceiving in females.\n• The decoction of the bark or root of Ficus religiosa is given in a dose of 50-60 ml to treat joint swelling and pain.\n• The tender leaves of Ashwattha and Vata (Ficus bengalensis) are made into paste and applied over the skin having black heads, pimples and discoloration to get the original skin tone.\n• Latex of Ficus religiosa is applied over wounds and bleeding spots to heal wound and control bleeding respectively.\n• Decoction of the bark of the tree is given in a dose of 50-60 ml to control diabetes and treat diseases of the vaginal tract.\n• The dried powder of the fruits or the paste of the bark is given with honey or sugar candy to treat severe cough.\n• Cold infusion or decoction prepared from the trunk bark or fruit of Ashwattha is consumed in a dose of 50 ml to treat skin diseases and acts like blood purifier.\n• In pregnant women, the decoction of the bark of Ficus religiosa helps in strengthening the uterine muscles and retains the fetus till full term."
+  },
+  {
+    "Number": 5,
+    "sanskrit_name": "Mrigashira",
+    "Botanical name": "Senegalia chundra\n(Roxb. ex Rottler) Maslin",
+    "English name": "Senegalia chundra",
+    "Malayalam Name": "Karingali",
+    "Kannada Name": "Kalu",
+    "Description": "Senegalia chundra is a small to medium-sized tree that can grow up to a certain height, typically characterized by its thorny branches and compound leaves. The leaves are bipinnate, which means they are composed of several small leaflets arranged on either side of a common axis. The tree produces yellow to golden-yellow flowers, which are small and fragrant, arranged in spherical heads.",
+    "Geographical distribution": "This species is native to the Indian subcontinent, where it thrives in dry and deciduous forests. It is well adapted to arid and semi-arid environments and can often be found in areas with well-drained soils.",
+    "Chemical constituents": "Senegalia chundra contains a variety of bioactive compounds, including tannins, which are prevalent in its bark and used for tanning leather and possessing antimicrobial properties. Flavonoids and phenolic acids contribute to its antioxidant and potential health benefits. Alkaloids and glycosides in the plant have been noted for their medicinal properties, such as analgesic and anti-inflammatory effects. Additionally, the heartwood and bark are sources of natural dyestuffs, used traditionally for producing red dye. These components together underline the plant's significance in traditional medicine, dye production, and the tanning industry.",
+    "Important Therapeutic use": "Senegalia chundra is traditionally used for its antimicrobial, anti-inflammatory, and antioxidant properties, aiding in treating infections, reducing inflammation, and preventing oxidative stress. Its astringent qualities are beneficial for gastrointestinal and dermatological issues, such as diarrhea, skin wounds, and infections. These uses are supported by its content of tannins, flavonoids, and other bioactive compounds.",
+    "Other medical uses": "Antimicrobial\nThe tannins and alkaloids present in the plant exhibit antimicrobial properties, making Senegalia chundra effective against a spectrum of bacterial and fungal pathogens. It's utilized in treating infections, especially in wound care and skin infections.\n\nAnti-inflammatory and Analgesic\nIts flavonoid content contributes to its anti-inflammatory and analgesic properties. This makes it useful in managing conditions like arthritis, where inflammation is a significant concern, by reducing swelling and pain.\n\nAntioxidant\nThe phenolic compounds and flavonoids offer antioxidant benefits, which are crucial in protecting the body from oxidative stress and related chronic diseases, including some forms of cancer and cardiovascular diseases.\n\nAstringent\nDue to its high tannin concentration, Senegalia chundra acts as a potent astringent. This property is harnessed in treating gastrointestinal issues like diarrhea and dysentery, as it helps tighten mucous membranes and reduce secretion.\n\nDermatological Applications\nIts antimicrobial and astringent effects are also applied in dermatology for treating wounds, ulcers, and skin infections, promoting faster healing and preventing microbial infections.\n"
+  }
+];
+
+export default plants;
--- oushadhavana/scripts/listing.js
+++ oushadhavana/scripts/listing.js
@@ -1,13 +1,14 @@
-window.onload = () => {
-  const listingParent = document.getElementById("oushadhavana-listing");
-  for(const plant of data) {
-    const listingChild = document.createElement("li");
-    const a = document.createElement('a');
-    const linkText = document.createTextNode(plant.name);
-    a.appendChild(linkText);
-    a.title = plant.name;
-    a.href = plant.link;
-    listingChild.appendChild(a);
-    listingParent.appendChild(listingChild);
+import plants from "../data.js";
+
+const { createApp, ref } = Vue;
+
+const app = {
+  setup() {
+    const plantsArray = ref(plants);
+    return {
+      plants: plantsArray
+    }
   }
-};
+}
+
+createApp(app).mount('#app')
