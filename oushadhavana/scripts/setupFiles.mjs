f6f573e38354a0f84b440a88f91a2da5fb31edef
Rakshith Bellare <rb@nuiteq.com>
2024-02-10 22:27:08 +0530

    oushadhavana-plant-details Only create the folder and index.html for each plant if the folder and file does not exists

--- oushadhavana/scripts/setupFiles.mjs
+++ oushadhavana/scripts/setupFiles.mjs
@@ -6,36 +6,48 @@ const pathOfFileToCopy = '../index.html';
 for(const plant of plants) {
   const folderPath = `../${plant['sanskrit_name'].toLowerCase().replace(/\s+/g, '')}`;
 
-  fs.mkdir(folderPath, (err) => {
+  fs.stat(folderPath, (err, stats) => {
     if (err) {
-      console.error('Error creating folder:', err);
-    } else {
-      console.log('Folder created successfully');
-
-      let fileContent;
-
-      fs.readFile(pathOfFileToCopy, 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error reading file:', err);
-          return;
-        }
-        fileContent = data;
-        console.log("read successful");
-
-        const filePath = `${folderPath}/index.html`;
-        console.log(typeof fileContent);
-
-        fs.writeFile(filePath, fileContent, (err) => {
+      if (err.code === 'ENOENT') {
+        fs.mkdir(folderPath, (err) => {
           if (err) {
-            console.error('Error creating file:', err);
+            console.error('Error creating folder:', err);
+            return;
           } else {
-            console.log('File created successfully');
+            let fileContent;
+
+            fs.readFile(pathOfFileToCopy, 'utf8', (err, data) => {
+              if (err) {
+                console.error('Error reading file:', err);
+                return;
+              }
+              fileContent = data;
+
+              const filePath = `${folderPath}/index.html`;
+
+              fs.writeFile(filePath, fileContent, (err) => {
+                if (err) {
+                  console.error('Error creating file:', err);
+                } else {
+                  console.log('File created successfully');
+                }
+              });
+            });
           }
         });
-      });
+      } else {
+        console.error('Error checking folder:', err);
+      }
+    } else {
+      if (stats.isDirectory()) {
+        console.log('Folder exists');
+        return;
+      } else {
+        console.log('Path exists, but is not a folder');
+        return;
+      }
     }
   });
-  console.log("file written");
-  console.log("==============");
+
 }
 
