13f9d47471f526e5a90a6dd5ff6549ccfc5f9126
deekshith.bellare <deekshith.bellare@theatom.app>
2024-02-25 17:15:48 +0530

    added comma

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -3,11 +3,11 @@ const plantImages = {
   "durva": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ],
   "khadirah": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ],
   "shami": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ],
-  "arka": [ "1.jpg", "2.jpg", "3.jpg", ],
+  "arka": [ "1.jpg", "2.jpg", "3.jpg",],
   "ashvatha": [ "1.jpg", "2.jpg", "3.jpg", ],
   "palasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ]
-  "panasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg","5.jpg"]
- "kadamba": [ "1.jpg", "2.jpg", "3.jpg"]
+  "panasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg","5.jpg"],
+ "kadamba": [ "1.jpg", "2.jpg", "3.jpg"],
  "udumbara": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg","5.jpg"]
 }
 
