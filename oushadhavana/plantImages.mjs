bd438fb0790ad8a01473ead826c3ecd7b4335a01
deekshith.bellare <deekshith.bellare@theatom.app>
2024-02-25 18:24:27 +0530

    updated amalaki details

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -9,7 +9,8 @@ const plantImages = {
   "panasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "kadamba": [ "1.jpg", "2.jpg", "3.jpg" ],
   "udumbara": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
-   "darbha": [ "1.jpg", "2.jpg", "3.jpg" ]
+   "darbha": [ "1.jpg", "2.jpg", "3.jpg" ],
+"amalaki": [ "1.jpg", "2.jpg", "3.jpg","4.jpg"],
 }
 
 
