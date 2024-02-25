c88d9bb92164097e7cdc3c0f5c548d3ec4090f1d
deekshith.bellare <deekshith.bellare@theatom.app>
2024-02-25 17:38:25 +0530

    updated the array with missing comma

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -5,10 +5,11 @@ const plantImages = {
   "shami": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ],
   "arka": [ "1.jpg", "2.jpg", "3.jpg",],
   "ashvatha": [ "1.jpg", "2.jpg", "3.jpg", ],
-  "palasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ]
-  "panasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg","5.jpg"],
- "kadamba": [ "1.jpg", "2.jpg", "3.jpg"],
- "udumbara": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg","5.jpg"]
+  "palasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ], // Added missing comma here
+  "panasa": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
+  "kadamba": [ "1.jpg", "2.jpg", "3.jpg" ],
+  "udumbara": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ]
 }
+
 
 export default plantImages;
