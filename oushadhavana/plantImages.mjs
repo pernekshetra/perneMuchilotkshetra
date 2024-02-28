19e67a7f467d806432dca96cacb76234f0e1c3b6
deekshith.bellare <deekshith.bellare@theatom.app>
2024-02-29 01:29:48 +0530

    updated renja details

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -25,7 +25,7 @@ const plantImages = {
   "amrataka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "bilva": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
    "sarala": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
-   "bakula": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ]
+   "bakula": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
 }
 
 
