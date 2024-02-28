290aa8cb3924771b4d3a520e72d867b0af57b30c
deekshith.bellare <deekshith.bellare@theatom.app>
2024-02-29 00:11:43 +0530

    plaksha images added

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -20,7 +20,8 @@ const plantImages = {
   "shami": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg" ],
   "sinshapa": [ "1.jpg", "2.jpg", "3.jpg" ],
   "udumbara": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
-  "vamsha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"]
+  "vamsha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "plaksha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"]
 }
 
 
