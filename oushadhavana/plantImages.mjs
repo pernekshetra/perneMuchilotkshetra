d92bb10af788ed38ba10abf2064cb0fed4b3ab82
deekshith.bellare <deekshith.bellare@theatom.app>
2024-02-29 00:26:17 +0530

    updated ambate plant details

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -21,7 +21,8 @@ const plantImages = {
   "sinshapa": [ "1.jpg", "2.jpg", "3.jpg" ],
   "udumbara": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "vamsha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-  "plaksha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"]
+  "plaksha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "amrataka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
 }
 
 
