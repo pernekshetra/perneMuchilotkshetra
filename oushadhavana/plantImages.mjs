e7c45b6ebe10a6f243a41ffb4aade8a83509c791
deekshith.bellare <deekshith.bellare@theatom.app>
2024-03-01 16:30:22 +0530

    kapitha updated

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -28,7 +28,7 @@ const plantImages = {
    "bakula": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
    "madhuka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
    "sarja": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
-   "kapittha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
+   "kapittha": [ "1.jpg", "2.jpg", "3.jpg"],
    "ashoka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
    "tuvaraka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
    "kolikukke": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
