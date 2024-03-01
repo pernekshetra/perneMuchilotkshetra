1f5ca70c73d5fc5021b86f2c192cc95b82ab5f9a
deekshith.bellare <deekshith.bellare@theatom.app>
2024-03-01 16:20:57 +0530

    updated arka

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -3,7 +3,7 @@ const plantImages = {
   "amra": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg" ],
   "apamarga": [ "1.jpg", "2.jpg", "3.jpg", ],
   "arjuna": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg" ],
-  "arka": [ "1.jpg", "2.jpg", "3.jpg" ],
+  "arka": [ "1.jpg", "2.jpg"],
   "ashvatha": [ "1.jpg", "2.jpg", "3.jpg" ],
   "darbha": [ "1.jpg", "2.jpg", "3.jpg" ],
   "durva": [ "1.jpg"],
