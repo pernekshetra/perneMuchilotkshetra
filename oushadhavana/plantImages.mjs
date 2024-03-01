f06102bc1344701885909a2f278d7b15d9ffeb5f
Rakshith Bellare <rb@nuiteq.com>
2024-03-01 18:53:16 +0530

    Fixed indentation

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -24,27 +24,27 @@ const plantImages = {
   "plaksha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
   "amrataka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "bilva": [ "1.jpg", "2.jpg", "3.jpg"],
-   "sarala": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
-   "bakula": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-   "madhuka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
-   "sarja": [ "1.jpg", "2.jpg", "3.jpg"],
-   "kapittha": [ "1.jpg", "2.jpg", "3.jpg"],
-   "ashoka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-   "tuvaraka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
-   "kolikukke": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "sarala": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
+  "bakula": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "madhuka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
+  "sarja": [ "1.jpg", "2.jpg", "3.jpg"],
+  "kapittha": [ "1.jpg", "2.jpg", "3.jpg"],
+  "ashoka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "tuvaraka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
+  "kolikukke": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
   "lakshmitaru": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "punnaga": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
-   "kumbhi": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-   "gambhari": [ "1.jpg", "2.jpg", "3.jpg"],
-   "agaru": [ "1.jpg", "2.jpg", "3.jpg"],
-   "chandana": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-   "champaka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-   "nagasampige": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
-    "vrikshamla": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"]
-   
+  "kumbhi": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "gambhari": [ "1.jpg", "2.jpg", "3.jpg"],
+  "agaru": [ "1.jpg", "2.jpg", "3.jpg"],
+  "chandana": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "champaka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "nagasampige": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "vrikshamla": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"]
 
-   
-   
+
+
+
 
    
 }
