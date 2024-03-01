b04d52b32c3e147f1c0f03e7977eab14b76cd37d
deekshith.bellare <deekshith.bellare@theatom.app>
2024-03-01 16:28:26 +0530

    vamsha

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -20,7 +20,7 @@ const plantImages = {
   "shami": [ "1.jpg", "2.jpg", "3.jpg"],
   "sinshapa": [ "1.jpg", "2.jpg", "3.jpg" ],
   "udumbara": [ "1.jpg", "2.jpg", "3.jpg"],
-  "vamsha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  "vamsha": [ "1.jpg", "2.jpg"],
   "plaksha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
   "amrataka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "bilva": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
