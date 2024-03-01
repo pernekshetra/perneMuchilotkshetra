01a8fe3d6cd25f77ac3f6b4d712b4fece269223a
deekshith.bellare <deekshith.bellare@theatom.app>
2024-03-01 16:36:47 +0530

    updated sarala

--- oushadhavana/plantImages.mjs
+++ oushadhavana/plantImages.mjs
@@ -24,7 +24,7 @@ const plantImages = {
   "plaksha": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
   "amrataka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
   "bilva": [ "1.jpg", "2.jpg", "3.jpg"],
-   "sarala": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
+   "sarala": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"],
    "bakula": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg"],
    "madhuka": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
    "sarja": [ "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" ],
